c9ad3281d1305f58f8080c8b97bfb0e79a9ad8b8
Viktoria <sasaika.tortilka@gmail.com>
2026-09-22 21:22:02 +0300

    capez potracheno

--- Kubenina_Viktoria/Life simulation.cpp
+++ Kubenina_Viktoria/Life simulation.cpp
@@ -9,6 +9,15 @@ struct Person {
 	RUB salary;
 	RUB from_mother;
 	RUB food;
+	RUB dorm;
+	RUB metro;
+	RUB merchandise;
+	RUB medicine;
+	RUB clothes;
+	RUB deutsch;
+	RUB therapist;
+	RUB presents;
+	RUB household;
 
 };
 
@@ -22,6 +31,16 @@ void vika_init()
 	vika.salary = 0;
 	vika.from_mother = 25'000;
 	vika.food = 15'000;
+	vika.dorm = 3300;
+	vika.merchandise = 500;
+	vika.medicine = 1500;
+	vika.clothes = 1000;
+	vika.deutsch = 9900;
+	vika.therapist = 8000;
+	vika.presents = 1300;
+	vika.household = 2000;
+
+
 }
 
 
@@ -53,21 +72,77 @@ void vika_food(const int year, int month)
 }
 
 
+void vika_dorm(const int year, int month)
+{
+	vika.cash -= vika.dorm;
+}
+
+void vika_metro(const int year, int month)
+{
+	vika.cash -= vika.metro;
+}
+
+void vika_merchandise(const int year, int month)
+{
+	vika.cash -= vika.merchandise;
+}
+
+void vika_medicine(const int year, int month)
+{
+	vika.cash -= vika.medicine;
+}
+
+void vika_clothes(const int year, int month)
+{
+	vika.cash -= vika.clothes;
+}
+
+void vika_deutsch(const int year, int month)
+{
+	vika.cash -= vika.deutsch;
+}
+
+void vika_therapist(const int year, int month)
+{
+	vika.cash -= vika.therapist;
+}
+
+
+void vika_presents(const int year, int month)
+{
+	vika.cash -= vika.presents;
+}
+
+
+void vika_household(const int year, int month)
+{
+	vika.cash -= vika.household;
+}
+
+
+
+
+
 void simulation()
 {
 	int year = 2026;
 	int month = 9;
-	while (not(year == 2026 and month == 11)) {
+	while (not(year == 2026 and month == 12)) {
 
 		vika_salary(year, month);
 		vika_from_mother(year, month);
 		vika_food(year, month);
-		//alice_car();
-		//alice_mortgage();
-		//alice_cat();
-		//alice_rent();
-		//alice_tax();
-		//alice_bank_inkome();
+		vika_dorm(year, month);
+		vika_metro(year, month);
+		vika_merchandise(year, month);
+		vika_medicine(year, month);
+		vika_clothes(year, month);
+		vika_deutsch(year, month);
+		vika_therapist(year, month);
+		vika_presents(year, month);
+		vika_household(year, month);
+
+		//bank_inkome();
 		//nalog_vb1chet
 
 		++month;
