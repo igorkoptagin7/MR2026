bbc03d751c4d037c68fc183caea42fb7a4e7d4b8
MolchanovIvan <spbpilot03@gmail.com>
2026-09-24 01:33:32 +0300

    Add files via upload

---
+++ Molchanov_Ivan/Simulation/alice's_reckoning_code.cpp
@@ -0,0 +1,119 @@
+#include <stdio.h>  //не запускается через run; 
+#include <stdlib.h>
+#include <time.h>
+using RUB = int;
+
+struct Person{
+    RUB cash;
+
+    //INCOME
+    RUB salary;
+
+    //EXPENDITURE
+    RUB rent;
+
+    //HAVING
+    bool having_car;
+};
+
+// FUNCTIONS 
+
+
+    //INIT
+void alice_init(Person& alice)
+{
+    alice.cash = 20'000; 
+    
+    //income init
+    alice.salary = 80'000;
+
+    //expenditure init
+    alice.rent = 30'000;
+
+    //having init
+    alice.having_car = true;
+}
+
+
+    //PRINT RESULT
+void print_output(Person& alice)        
+{
+    printf("Alice's cash: %d\n", alice.cash); 
+}
+
+
+    //RANDOM FUNCTIONS
+
+void car_breaking(Person& alice) //CAR BREAKING RANDOM
+{
+    if (rand() % 100 < 10){ //10% chance of breaking
+        alice.cash -= 50'000;
+    }
+}
+
+
+    //INCOME
+
+void alice_salary(Person& alice) //SALARY CALCULATION
+{
+    alice.cash += alice.salary;
+}
+
+    //EXPENDITURE
+
+void alice_rent(Person& alice) //RENT CALCULATION
+{
+    //rent
+    alice.cash -= alice.rent;
+}
+    
+void alice_car(Person& alice) //CAR CALCULATION
+{
+    if (alice.having_car == true){
+        alice.cash -= 10'000;
+        car_breaking(alice);
+    }
+}
+
+
+// SIMULATION 
+
+void simulation(Person& alice)
+{
+    char month = 9;
+    unsigned short year = 2026;
+    while (!(year == 2027 && month == 9)){  //не работает not и and
+        
+        //Alice's Functions + $$$
+
+        alice_salary(alice);
+
+        //Alice's Functions - $$$
+
+        alice_rent(alice);
+        alice_car(alice);
+    
+        month++;
+        if (month == 13){
+            year++;
+            month = 1;
+        }
+    }
+
+}
+
+//SIMULATION 
+
+// MAIN 
+int main()
+{
+    srand(time(NULL));
+    
+    struct Person alice;
+
+    alice_init(alice);
+    
+    simulation(alice);
+
+    print_output(alice);
+}
