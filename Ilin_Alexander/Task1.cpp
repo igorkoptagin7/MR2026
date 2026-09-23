004eaa3fc94a59366e705b011387741842039106
Sanya-Ilin-P <Sanya.ilin.5564_3@inbox.ru>
2026-09-23 23:11:09 +0300

    Create new functions

--- Ilin_Alexander/Task1.cpp
+++ Ilin_Alexander/Task1.cpp
@@ -3,10 +3,18 @@
 #include <random>
 #include <string>
 
+using LIFE = unsigned int;
+
 using RUB = unsigned long int;
 
 const int end_month = 7;
 const int end_year = 2036;
+
+int month = 1;
+int year = 2026;
+
+static std::random_device seed;
+static std::mt19937 generator(seed());
 
 class Person 
 {
@@ -15,29 +23,43 @@ public:
     bool life = true;
 // Животное
     bool animal = false;                // Подумать о создании нового класса...
-    RUB animal_cost = 2'000;
-
+    RUB animal_cost = 10'000;
 // Деньги
     RUB money = 10'000;
     RUB safety_pillow = 50'000;
 // Банкиры
     RUB bank_money = 0;
+    float bank_percent = 0.3;
+    RUB total_spent = 0;
 // Доходы
-    RUB salary = 30'000;
+    RUB salary = 10'000;
 // Траты
-    RUB spents_food = 20'000;
-    RUB spents_internet = 1'000;
+    RUB spents_food = 8'000;
+    RUB spents_internet = 1'500;
 
-    void money_profit(const RUB profit)                                 // Изменение бюджета
+    RUB random_digital(const RUB x, const RUB y)
+    {
+        std::uniform_int_distribution<RUB> cost(x, y);
+        return cost(generator);
+    }
+
+    void money_profit(const RUB profit)                                 // Изменение бюджета в плюс
     {
         money += profit;
+    }
+    void money_less(const RUB profit)                                   // Изменение бюджета в минус
+    {
+        money -= profit;
     }
 
     void money_count()                                                  // Выписка бюджета
     {
         if (life){
             printf("My money                   : %lu\n", money);
-            printf("My money in bank           : %lu\n", bank_money);            
+            printf("My money in bank           : %lu\n", bank_money);
+            if (animal){
+                printf("You have a animal\n");
+            }
         }
         else{
             printf("U are bankrupt :( ");
@@ -47,11 +69,11 @@ public:
     bool check_money(const RUB cost)                                    // Проверка на хватку денег (Выдает тру/фолс)
     {
         bool verified = false;
-        if (cost > money){
-            if (cost <= money + bank_money){
+        if (cost < money){
+            if (cost <= (money + bank_money)){
                 verified = true;
             }
-            if (cost > money + bank_money){
+            if (cost > (money + bank_money)){
                 life = false;
             }
         }
@@ -59,17 +81,39 @@ public:
     }
 
     bool check_interest(const RUB cost)                                 // Проверка на заинтересованность в покупке (Выдает тру/фолс)
-    {                                                                   // Добавить рандомную оценку на относительной цене покупки к бюджету общему
-        bool verified = false;
-        if (true){
-            verified = true;
-        }
+    {   
+        bool verified = false;                                          // verified - проверено              
+        if(bank_money > 0){
+            float interest = static_cast<float>(cost) / static_cast<float>(bank_money);                           
+            if ((0 < interest) && (interest <= 0.1)){       // Высокий интерес
+                verified = true;
+            }
+            if ((0.1 < interest) && (interest <= 0.3)){     // Средний интерес
+                if (random_digital(0, 100) < 50){
+                    verified = true;              
+                }
+                else{
+                    verified = false;
+                }
+            }
+            if ((0.3 < interest) && (interest <= 0.5)){     // Низкий интерес
+                if (random_digital(0, 100) < 10){
+                    verified = true;              
+                }
+                else{
+                    verified = false;
+                }
+            }
+            if (interest > 0.5){            // Отсутствие интереса
+                verified = false;
+            }
+        }    
         return verified;
     }
 
-    void salary()                                                      // Зарплата
+    void get_salary()                                                      // Зарплата
     {
-        if (this -> salary){
+        if (salary){
         money_profit(salary); 
         }
     }
@@ -77,7 +121,7 @@ public:
     void buy_animal()
     {
         animal = true;
-        money_profit(-animal_cost);
+        money_less(animal_cost);    
     }
 
     void shopping()                                                     // Траты на еду
@@ -90,12 +134,14 @@ public:
             cost += spents_internet;
         }
         if(check_money(cost)){
-            money_profit(-cost); 
+            money_less(cost); 
         }
 
-        if (!animal and check_money(animal_cost) and check_interest(animal_cost)){
+        if (!animal and check_money(animal_cost) && check_interest(animal_cost)){
             buy_animal();
+            cost += animal_cost;
         }
+        total_spent = cost;
     }
 
     void cost_save()                                                    // Сбережения
@@ -105,29 +151,53 @@ public:
             bank_money += transfer;
             money -= transfer;
         }
-    }   
+    } 
+    
+    void bank_percent_profit()
+    {
+        bank_percent = 0.1;
+        
+        if (total_spent >= 5'000){
+            bank_percent += 0.07;
+        }
+        money += static_cast<RUB>(static_cast<float>(bank_money) * (bank_percent / 12));
+    }
+
 };
+/*
+class Animal
+{
+    public:
+    LIFE hunger = 5;
+
+    void eating()
+    {
+        if (){
+            hunger -= 1
+        }
+        if (hunger)
+    }
+}
+*/
 
 Person I{};
 
 void simulation(Person& person)
 {
-    int month = 1;
-    int year = 2026;
-
     for(; year <= end_year; year++){                                    // Ежегодно
 
         for (; month <= 12 ; month++){                                  // Ежемесячно
                 if(year == end_year && month == end_month){
-                    person.money_count(); 
-                    break;
+                    person.money_count();
+                    return;
                 }
                 
-                person.salary(); 
+                person.get_salary(); 
 
                 person.shopping();
 
                 person.cost_save();
+                person.bank_percent_profit();
         }
         month = 1;
     }
